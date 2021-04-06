dcb0c58031a82fb863bf2591ce39bd833f041bde
elek34ka <xtkcbfgkqwerty@gmail.com>
2021-04-06 20:04:03 +0300

    Complete first task

--- jetbrains-task1-lego/src/Main.java
+++ jetbrains-task1-lego/src/Main.java
@@ -3,52 +3,54 @@ import java.util.Scanner;
 public class Main {
     static Scanner in = new Scanner(System.in);
 
-    static int[] use = new int[26];
-    static int[][] a = new int[26][26];
+    static final int ALPHABET_SIZE = 26;
+
+    static int[] use_dfs = new int[ALPHABET_SIZE];
+    static boolean[][] arr = new boolean[ALPHABET_SIZE][ALPHABET_SIZE];
     static int n;
-    static String[] str;
-    static boolean[] used = new boolean[26];
+    static String[] words;
+    static boolean[] used = new boolean[ALPHABET_SIZE];
 
     static boolean dfs(int u) {
-        use[u] = 1;
-        for (int i = 0; i < 26; i++) {
-            if (a[u][i] == 1) {
-                if (use[i] == 0) {
+        use_dfs[u] = 1;
+        for (int i = 0; i < ALPHABET_SIZE; i++) {
+            if (arr[u][i]) {
+                if (use_dfs[i] == 0) {
                     if (dfs(i))
                         return true;
-                } else if (use[i] == 1) {
+                } else if (use_dfs[i] == 1) {
                     return true;
                 }
             }
         }
-        use[u] = 2;
+        use_dfs[u] = 2;
         return false;
     }
 
     static void read() {
         n = in.nextInt();
         in.nextLine();
-        str = new String[n];
+        words = new String[n];
         for (int i = 0; i < n; i++) {
-            str[i] = in.nextLine();
+            words[i] = in.nextLine();
         }
     }
 
     static void createGraph() {
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < str[i].length(); j++) {
+            for (int j = 0; j < words[i].length(); j++) {
                 for (int k = i + 1; k < n; k++) {
-                    if (j >= str[k].length())
+                    if (j >= words[k].length())
                         break;
-                    if (str[i].substring(0, j).equals(str[k].substring(0, j))) {
-                        if (str[i].charAt(j) != str[k].charAt(j)) {
-                            int x = str[i].charAt(j) - 'a';
-                            int y = str[k].charAt(j) - 'a';
-                            if (a[y][x] == 1) {
+                    if (words[i].substring(0, j).equals(words[k].substring(0, j))) {
+                        if (words[i].charAt(j) != words[k].charAt(j)) {
+                            int x = words[i].charAt(j) - 'a';
+                            int y = words[k].charAt(j) - 'a';
+                            if (arr[y][x]) {
                                 System.out.println("Impossible");
                                 System.exit(0);
                             }
-                            a[x][y] = 1;
+                            arr[x][y] = true;
                         }
                     } else {
                         break;
@@ -59,10 +61,10 @@ public class Main {
     }
 
     static void printGraph() {
-        for (int i = 0; i < 26; i++) {
+        for (int i = 0; i < ALPHABET_SIZE; i++) {
             System.out.print((char) (i + 'a') + " > ");
-            for (int j = 0; j < 26; j++) {
-                if (a[i][j] == 1)
+            for (int j = 0; j < ALPHABET_SIZE; j++) {
+                if (arr[i][j])
                     System.out.print((char) (j + 'a'));
             }
             System.out.println();
@@ -70,8 +72,8 @@ public class Main {
     }
 
     static void findCircle() {
-        for (int i = 0; i < 26; i++) {
-            if (use[i] == 0) {
+        for (int i = 0; i < ALPHABET_SIZE; i++) {
+            if (use_dfs[i] == 0) {
                 if (dfs(i)) {
                     System.out.println("Impossible");
                     System.exit(0);
@@ -82,12 +84,12 @@ public class Main {
 
     static String makeAnswer() {
         StringBuilder ans = new StringBuilder();
-        for (int o = 0; o < 26; o++) {
-            for (int i = 0; i < 26; i++) {
+        for (int o = 0; o < ALPHABET_SIZE; o++) {
+            for (int i = 0; i < ALPHABET_SIZE; i++) {
                 int k = 0;
-                for (int j = 0; j < 26; j++) {
-                    if (!used[j])
-                        k += a[i][j];
+                for (int j = 0; j < ALPHABET_SIZE; j++) {
+                    if (!used[j] && arr[i][j])
+                        k++;
                 }
                 if (k == 0 && !used[i]) {
                     ans.append((char) (i + 'a'));
